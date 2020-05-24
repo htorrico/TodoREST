920ba38ae7116eccaa7d00a8a724adb00708d98a
Hugo Felipe Torrico Marquez <htorrico@truenorthcorporation.com>
2020-05-23 19:56:02 -0500

    Agregar archivos de proyecto.

---
+++ TodoREST/Constants.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace TodoREST
+{
+    public static class Constants
+    {
+        // URL of REST service
+        //public static string RestUrl = Device.RuntimePlatform == Device.Android ? "https://10.0.2.2:5001/api/todoitems/{0}" : "https://localhost:5001/api/todoitems/{0}";
+        public static string RestUrl = "http://192.168.1.5/todoapi/api/todoitems/{0}";
+    }
+}
