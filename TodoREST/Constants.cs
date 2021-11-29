85883aea87da7e6fe71221b5fc8c96f720f8a931
Hugo Felipe Torrico Marquez <htorrico@tecsup.edu.pe>
2021-11-29 14:13:29 -0500

    Cambiar link de API

--- TodoREST/Constants.cs
+++ TodoREST/Constants.cs
@@ -8,7 +8,10 @@ namespace TodoREST
         //public static string RestUrl = Device.RuntimePlatform == Device.Android ? "https://10.0.2.2:5001/api/todoitems/{0}" : "https://localhost:5001/api/todoitems/{0}";
         //http://colegioquimico-001-site6.dtempurl.com/api/todoitems/GET
         //public static string RestUrl = "http://192.168.1.10/todoapi/api/todoitems/{0}";
-        public static string RestUrl = "http://colegioquimico-001-site6.dtempurl.com/api/todoitems/{0}";
+        //public static string RestUrl = "http://192.168.43.146/TodoApi/api/todoitems/{0}";
+        //public static string RestUrl = "http://colegioquimico-001-site6.dtempurl.com/api/todoitems/{0}";
+        public static string RestUrl = "http://sapientiaperu-001-site3.dtempurl.com/api/todoitems/{0}";
+        
 
 
     }
